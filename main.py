46efb56323dae49638b83e1e5c776b2100c4d11a
jbermejo14 <101327815+jbermejo14@users.noreply.github.com>
2023-08-18 18:16:49 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,81 +1,289 @@
 # IMPORTS
 import pygame
-import button
 import sys
+import time
 
-
-gameDisplay = pygame.display.set_mode((600, 600))
+# IMAGES GENERATION
 bg = pygame.image.load("images/bg.png")
-daman = pygame.image.load("images/dama_roja.jpg")
+damar = pygame.image.load("images/dama_roja.jpg")
+damab = pygame.image.load("images/dama_blue.jpg")
+damar2 = pygame.image.load("images/dama_roja2.jpg")
+damab2 = pygame.image.load("images/dama_blue2.jpg")
 
-white = pygame.Color(255, 255, 255)
+
+# COLOR GENERATION
 black = pygame.Color(0, 0, 0)
 red = pygame.Color(255, 0, 0)
-green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
+blue_turn = True
+red_turn = False
 
-# DISPLAY GENERTATION
+
+# DISPLAY GENERATION
+gameDisplay = pygame.display.set_mode((900, 600))
 pygame.init()
 pygame.display.set_caption("Damas")
 gameExit = False
+gameDisplay.fill('white')
 
-
+pygame.draw.rect(gameDisplay, black, (750, 0, 150, 600))
+pygame.draw.rect(gameDisplay, black, (0, 0, 150, 600))
+# SQUARE CLASS
 class Square:
-    def __init__(self, posy, posx):
+
+
+    def __init__(self, posy, posx, pulsado):
+        self.pulsado = pulsado
+        self.top_rect = pygame.Rect((posy, posx),(75, 75))
         self.posy = posy
         self.posx = posx
-        self.rect = pygame.draw.rect(gameDisplay, white, (self.posy, self.posx, 75, 75))
+        self.rect = pygame.draw.rect(gameDisplay, black, (self.posy, self.posx, 75, 75))
 
+
+    def check_click(self):
+        posm = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(posm):
+            if pygame.mouse.get_pressed()[0] is True:
+                self.select()
+                
+
+    def select(self):
+
+        self.pulsado = True
+        if self.pulsado == True:
+            for i in square_list:
+                if i.pulsado is True:
+                    if i is not self:
+                        i.pulsado = False
+
+
+        for i in dama_list:
+            if i.selected is True:
+                var1 = 1
+                for b in square_list:
+                    if b in i.list:
+                        if b is self:
+                            global red_turn, blue_turn
+                            aux_posy = self.posy
+                            aux_posx = self.posx
+                            name = 's' + str(len(square_list) + 1)
+                            name = Square(i.posy, i.posx, False)
+                            square_list.append(name)
+                            i.posx = aux_posx
+                            i.posy = aux_posy
+                            i.top_rect = pygame.Rect((self.posy, self.posx),(75, 75))
+                            if blue_turn is True:
+                                    red_turn = True
+                                    blue_turn = False
+                            elif blue_turn is False:
+                                    red_turn = False
+                                    blue_turn = True
+
+
+
+                    if b in i.list2:
+                        if b is self:
+                            aux_posy = self.posy
+                            aux_posx = self.posx
+                            name = 's' + str(len(square_list) + 1)
+                            name = Square(i.posy, i.posx, False)
+                            square_list.append(name)
+                            i.posx = aux_posx
+                            i.posy = aux_posy
+                            i.top_rect = pygame.Rect((self.posy, self.posx),(75, 75))
+
+
+# DAMA CLASS
 class Dama:
-    def __init__(self, posy, posx):
+
+    def __init__(self, posy, posx, color, selected):
+        self.list = []
+        self.list2 = []
+        self.selected = selected
         self.posy = posy
         self.posx = posx
+        self.top_rect = pygame.Rect((self.posy, self.posx),(75, 75))
+        self.color = color
 
-    def creation(self):
-        gameDisplay.blit(daman, (self.posy, self.posx, 10, 10))
+        if self.color is red:
+            gameDisplay.blit(damar, (self.posy, self.posx, 10, 10))
+        elif self.color is blue:
+            gameDisplay.blit(damab, (self.posy, self.posx, 10, 10))
+
         pygame.display.update()
     
-    def clicked(self):
+
+    def check_click(self):
+
         posm = pygame.mouse.get_pos()
-        print(posm)
+        if self.top_rect.collidepoint(posm):
+            if pygame.mouse.get_pressed()[0] is True:
+                if self.color is red:
+                    if red_turn is True:
+                        self.select()
+                elif self.color is blue:
+                    if blue_turn is True:  
+                        self.select()           
+            else:
+                if self.color is red:
+                    gameDisplay.blit(damar, (self.posy, self.posx, 10, 10))
+                elif self.color is blue:
+                    gameDisplay.blit(damab, (self.posy, self.posx, 10, 10))
+                pygame.display.update()
 
+        
+    def get_list(self):
+            
+            pos_list = []
+            if self.color is blue:
+                pos_list.append((self.posy - 75, self.posx - 75))
+                pos_list.append((self.posy + 75, self.posx - 75))
+            elif self.color is red:
+                pos_list.append((self.posy + 75, self.posx + 75))
+                pos_list.append((self.posy - 75, self.posx + 75))
+            return pos_list
 
-def check_creation():
-    y = 0
-    x = 0
+    def get_list2(self):
+            
+            pos_list = []
+            if self.color is red:
+                pos_list.append((self.posy - 75, self.posx - 75))
+                pos_list.append((self.posy + 75, self.posx - 75))
+            elif self.color is blue:
+                pos_list.append((self.posy + 75, self.posx + 75))
+                pos_list.append((self.posy - 75, self.posx + 75))
+            return pos_list
 
-    # ROW 0 
-    for i in range(4):
-        i = Dama(y, x)
-        i.creation()
-        y = y + 150
+    def select(self):
+        self.selected = True
+        aux_posx = ''
+        aux_posy = ''
+        self.list = self.get_list()
+        for i in dama_list:
+            aux_posy = i.posy
+            aux_posx = i.posx
+            if self.list[0] == (aux_posy, aux_posx):
+                self.list[0] = i
+            elif self.list[1] == (aux_posy, aux_posx):
+                self.list[1] = i
+
+        for i in square_list:
+            aux_posy2 = i.posy
+            aux_posx2 = i.posx
+            if self.list[0] == (aux_posy2, aux_posx2):
+                self.list[0] = i
+            elif self.list[1] == (aux_posy2, aux_posx2):
+                self.list[1] = i
+
+        # TODO FINISH DAMA TOUCHING AND EATING
+        for i in self.list:
+            if isinstance(i, Dama) is True:
+                self.list2 = i.get_list2()
+                for i in square_list:
+                    aux_posy2 = i.posy
+                    aux_posx2 = i.posx
+                    if self.list2[0] == (aux_posy2, aux_posx2):
+                        self.list2[0] = i
+                    elif self.list2[1] == (aux_posy2, aux_posx2):
+                        self.list2[1] = i
+
+        if pygame.mouse.get_pressed()[0] is True:
+            self.selected = True
+
+        if self.selected == True:
+        
+            if self.color is red:
+                gameDisplay.blit(damar2, (self.posy, self.posx, 10, 10))
+            elif self.color is blue:
+                gameDisplay.blit(damab2, (self.posy, self.posx, 10, 10))
+            
+            for i in dama_list:
+                if i.selected is True:
+                    if i is not self:
+                        i.selected = False
+                          
         pygame.display.update()
-        print(i.posy, i.posx)
 
-def board_creation():
-    y = 0
-    x = 0
 
-    # ROW 0 
-    for i in range(4):
-        i = Square(y, x)
-        i.rect
-        y = y + 150
-        pygame.display.update()
-        print(i.posy, i.posx)
 
-board_creation()
-# check_creation()
 
-daman1 = Dama
+# RED DAMA CREATION
+dr1 = Dama(225, 0, red, False)
+dr2 = Dama(375, 0, red, False)
+dr3 = Dama(525, 0, red, False)
+dr4 = Dama(675, 0, red, False)
+dr5 = Dama(150, 75, red, False)
+dr6 = Dama(300, 75, red, False)
+dr7 = Dama(450, 75, red, False)
+dr8 = Dama(600, 75, red, False)
+dr9 = Dama(225, 150, red, False)
+dr10 = Dama(375, 150, red, False)
+dr11 = Dama(525, 150, red, False)
+dr12 = Dama(675, 150, red, False)
+
+
+# BLUE DAMA CREATION
+db1 = Dama(150, 375, blue, False)
+db2 = Dama(300, 375, blue, False)
+db3 = Dama(450, 375, blue, False)
+db4 = Dama(600, 375, blue, False)
+db5 = Dama(225, 450, blue, False)
+db6 = Dama(375, 450, blue, False)
+db7 = Dama(525, 450, blue, False)
+db8 = Dama(675, 450, blue, False)
+db9 = Dama(150, 525, blue, False)
+db10 = Dama(300, 525, blue, False)
+db11 = Dama(450, 525, blue, False)
+db12 = Dama(600, 525, blue, False)
+
+
+# BOARD CREATION
+s1 = Square(150, 225, False)
+s2 = Square(300, 225, False)
+s3 = Square(450, 225, False)
+s4 = Square(600, 225, False)
+s5 = Square(225, 300, False)
+s6 = Square(375, 300, False)
+s7 = Square(525, 300, False)
+s8 = Square(675, 300, False)
+
+
+# SQUARE LIST
+square_list = [s1, s2, s3, s4, s5, s6, s7, s8]
+
+# ALL DAMA LIST
+dama_list = [dr1, dr2, dr3, dr4, dr5, dr6, dr7, dr8, dr9, dr10, dr11, dr12, db1, db2, db3, db4, db5, db6, db7, db8, db9, db10, db11, db12]
+
+# RED DAMA LIST
+damar_list = [dr1, dr2, dr3, dr4, dr5, dr6, dr7, dr8, dr9, dr10, dr11, dr12]
+
+# BLUE DAMA LIST 
+damab_list = [db1, db2, db3, db4, db5, db6, db7, db8, db9, db10, db11, db12]
+
+
+def damab_click_checks():
+
+    for i in dama_list:
+        i.check_click()
+
+
+def board_click_checks():
+
+    for i in square_list:
+        i.check_click()
+
+
+pygame.display.update()
+
 
 while not gameExit:
-    posm = pygame.mouse.get_pos()
-    print(posm)
+
+    board_click_checks()
+    damab_click_checks()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            posm == 
+
+#TODO FINISH TURNOS
